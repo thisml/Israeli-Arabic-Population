55ea421fd99877e1731f66435edbf5eac0c34c3f
oba2311 <oba2311@gmail.com>
2018-11-12 14:49:06 +0000

    wiki scraping added

---
+++ getData.py
@@ -0,0 +1,8 @@
+import wikitablescrape
+
+wikitablescrape.scrape(
+    url="https://en.wikipedia.org/wiki/Population_statistics_for_Israeli_settlements_in_the_West_Bank",
+    output_name="SettlementsPopulation"
+)
+
+# see: https://github.com/rocheio/wiki-table-scrape
